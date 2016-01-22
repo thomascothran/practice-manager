abc6e019b5acdee9d4862d6d7f093dcee90e5645
tmcothran <tmcothran@hotmail.com>
2016-01-21 20:46:02 -0500

    In the tasks application, removed references in admin.py to the old contexts attributes that tasks and properties used to have. This would cause errors, since Task and Project no longer have those attributes.

--- tasks/admin.py
+++ tasks/admin.py
@@ -11,19 +11,19 @@ class TaskAdmin(admin.ModelAdmin):
     """
     fieldsets = [
         (None,          {'fields': ['name', 'notes']}),
-        ('Tags',        {'fields': ['context', 'priority', 'status']}),
+        ('Tags',        {'fields': ['status']}),
         ('Reminders',   {'fields' : ['due_date'],
                          'classes' : ['collapse']}),
         ('Related Projects', {'fields': ['related_project'],
                               'classes': ['collapse']})
     ]
 
-    list_display = ['name', 'context', 'priority', 'status', 'due_date']
+    list_display = ['name', 'status', 'due_date']
     """
     Controls what shows up in the admin user interface table
     """
 
-    list_filter = ['context', 'priority', 'due_date', 'status']
+    list_filter = ['due_date', 'status']
 
     search_fields = ['name', 'notes']
 
@@ -33,14 +33,14 @@ class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,          {'fields': ['name']}),
         ('Details',     {'fields': ['purpose', 'vision', 'big_steps']}),
-        ('Tags',        {'fields': ['context', 'priority', 'status']}),
+        ('Tags',        {'fields': ['status']}),
         ('Dates',       {'fields': ['due_date'], 'classes': ['collapse']}),
         ('Related Projects', {'fields': ['related_project']}),
     ]
 
-    list_display = ['name', 'context', 'priority', 'status', 'due_date']
+    list_display = ['name', 'status', 'due_date']
 
-    list_filter = ['context', 'priority', 'due_date', 'status']
+    list_filter = ['due_date', 'status']
 
     search_fields = ['name', 'purpose', 'vision', 'big_steps']
 
