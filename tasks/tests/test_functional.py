27ccb3ed9feaae405220b7012df32d15f1445103
tmcothran <tmcothran@hotmail.com>
2016-01-28 14:19:26 -0500

    In tasks.tests.test_functional.py, created first functional test (just to see if page pulls up) for the Task Manager

---
+++ tasks/tests/test_functional.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import unittest
+
+from django.core.urlresolvers import reverse
+
+class BasicTest(unittest.TestCase):
+    """
+    This class just checks to make sure that the basics of the task
+    manager are up and working.
+    """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(4)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000/case-manager')
+        self.assertIn('Cothran', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
