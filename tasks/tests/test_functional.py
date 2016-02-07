b6df00a401ee42f382b8d5f6b6c2f3723a38eb40
tmcothran <tmcothran@hotmail.com>
2016-02-06 20:00:45 -0500

    In the functional tests in the task manager, switched to StaticLiveServerTestCase

--- tasks/tests/test_functional.py
+++ tasks/tests/test_functional.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 import unittest
 
 from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-class BasicTest(unittest.TestCase):
+class BasicTest(StaticLiveServerTestCase):
     """
     This class just checks to make sure that the basics of the task
     manager are up and working.
@@ -19,6 +21,8 @@ class BasicTest(unittest.TestCase):
     def test_title(self):
         self.browser.get('http://localhost:8000/case-manager')
         self.assertIn('Cothran', self.browser.title)
+
+
 
 
 if __name__ == '__main__':
