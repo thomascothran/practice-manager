e105b17128b61ec4a5e139a8b4120e9e50f805d6
tmcothran <tmcothran@hotmail.com>
2016-03-10 19:51:22 -0500

    Added more tests for file manager note index view. Tests passing

--- file_manager/tests/test_functional.py
+++ file_manager/tests/test_functional.py
@@ -1,10 +1,17 @@
 from django.test import Client, TestCase
 from django.contrib.staticfiles.testing import LiveServerTestCase
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 from selenium import webdriver
 
 import unittest
+
+# Constants
+test_superuser_username = 'test_sup_aj3j*(Y'
+test_superuser_email = 'test_sup_234@gmail.com'
+test_superuser_password = 'sdlfkj3232()*HT^'
 
 
 class SeleniumTests(TestCase, LiveServerTestCase):
@@ -13,8 +20,50 @@ class SeleniumTests(TestCase, LiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(5)
 
+        # Create objects for testing
+        User.objects.create_superuser(
+            username=test_superuser_username,
+            email=test_superuser_email,
+            password=test_superuser_password
+        )
+
     def tearDown(self):
         self.browser.quit()
+
+    # Helper functions
+    def log_user_in(self, user_object, password):
+        """
+        This is designed to be a helper function that logs the user in.
+        :param user_object: this is the user
+        :param password: this is a string representing the password
+        :return:
+        """
+        # First, check to see whether username and password authenticate directly
+        # using authenticate()
+        user_auth = authenticate(username=user_object.username, password=password)
+        self.assertTrue(
+            # If user couldn't be authenticated, user_auth returns None
+            user_auth is not None,
+            msg=('User authentication failed in test_functional.log_user_in() when' +
+                 'it was attempted to login user with authenticate()')
+        )
+        # Now check to make sure that the test would fail with bad credentials
+        user_not_auth = authenticate(username='absdflkjewrw2', password='werjksler3#@')
+        self.assertFalse(
+            user_not_auth is not None,
+            msg='For some reason, attempt to authenticate non-logged in user succeeded.'
+        )
+        # Try to pull up task index, make sure you get redirected
+        self.browser.get(str(self.live_server_url) + reverse('file_manager:note_index'))
+        self.assertTrue('login' in self.browser.current_url)
+        self.browser.implicitly_wait(10)
+        # Login
+        self.browser.find_element_by_name('username').send_keys(user_object.username)
+        self.browser.find_element_by_name('password').send_keys(password)
+        self.browser.find_element_by_name('submit').click()
+        self.browser.implicitly_wait(10)
+        return user_object
+
 
     def test_the_file_manager_index_redirected_anon_users(self):
         self.browser.get(self.live_server_url + reverse('file_manager:note_index'))
@@ -23,6 +72,30 @@ class SeleniumTests(TestCase, LiveServerTestCase):
             'login' in self.browser.current_url,
             msg='Current url is %s' % self.browser.current_url,
         )
+
+    def test_that_authorized_users_can_reach_note_index_page(self):
+        # Pull authorized users from database by putting each in a
+        # dict of the user object and the password
+        local_test_superuser = {
+            'user': User.objects.get(username=test_superuser_username),
+            'password': test_superuser_password
+        }
+
+        authorized_users = [local_test_superuser]
+        # Loop through authorized users, seeing if they can get to the task page
+        for authorized_user in authorized_users:
+            self.log_user_in(
+                user_object=authorized_user['user'],
+                password=authorized_user['password']
+            )
+            self.browser.get(self.live_server_url + reverse('file_manager:note_index'))
+            # Assert that we have reached the note index page
+            self.assertEqual(
+                self.browser.current_url,
+                str(self.live_server_url + reverse('file_manager:note_index')),
+                msg='User %s could not reach note index page' % authorized_user['user']
+            )
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
