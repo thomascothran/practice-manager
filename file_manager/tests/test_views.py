1d6e9eb6eb5c633d8166efb272e000cc06189f2e
tmcothran <tmcothran@hotmail.com>
2016-06-15 19:45:05 -0400

    Added requirements.txt, fixed some formatting.

--- file_manager/tests/test_views.py
+++ file_manager/tests/test_views.py
@@ -15,6 +15,7 @@ test_superuser_email = 'tec@gmail.com'
 
 test_note_title = 'test_note_324jsfO#IJ@4'
 
+
 class NoteIndexViewTests(TestCase):
 
     def setUp(self):
@@ -35,11 +36,16 @@ class NoteIndexViewTests(TestCase):
         test_superuser = User.objects.get(username=test_superuser_username)
         client.force_login(user=test_superuser)
         response = client.get(reverse('file_manager:note_index'), follow=True)
-        self.assertTemplateUsed(response=response, template_name='file_manager/note_index.html')
+        self.assertTemplateUsed(
+            response=response,
+            template_name='file_manager/note_index.html'
+        )
 
     def test_that_authorized_users_can_see_note_index(self):
         # Make list of authorized users
-        local_test_superuser = User.objects.get(username=test_superuser_username)
+        local_test_superuser = User.objects.get(
+            username=test_superuser_username
+        )
         authorized_users = [local_test_superuser]
 
         client = Client()
@@ -51,7 +57,8 @@ class NoteIndexViewTests(TestCase):
     def test_that_anonymous_users_cannot_see_note_index(self):
         client = Client()
         response = client.get(reverse('file_manager:note_index'), follow=True)
-        url_redirect = '/accounts/login/?next=%s' % reverse('file_manager:note_index')
+        url_redirect = ('/accounts/login/?next=%s' %
+                        reverse('file_manager:note_index'))
         self.assertRedirects(response=response, expected_url=url_redirect)
 
 
@@ -82,7 +89,8 @@ class NoteCreateView(TestCase):
     def tests_whether_anonymous_users_are_redirected(self):
         client = Client()
         response = client.get(reverse('file_manager:note_create'))
-        expected_url = '/accounts/login/?next=%s' % reverse('file_manager:note_create')
+        expected_url = ('/accounts/login/?next=%s' %
+                        reverse('file_manager:note_create'))
         self.assertRedirects(
             response=response,
             expected_url=expected_url
@@ -91,7 +99,9 @@ class NoteCreateView(TestCase):
     def tests_whether_authorized_users_have_access_to_note_creation_view(self):
         client = Client()
         # Make a list of authorized users
-        local_test_superuser = User.objects.get(username=test_superuser_username)
+        local_test_superuser = User.objects.get(
+            username=test_superuser_username
+        )
         authorized_users = [local_test_superuser]
         # See if authorized users have access
         for authorized_user in authorized_users:
@@ -123,18 +133,32 @@ class NoteDetailsViewTest(TestCase):
     def test_whether_task_details_uses_correct_template(self):
         client = Client()
         # Pull objects from db
-        local_test_superuser = User.objects.get(username=test_superuser_username)
+        local_test_superuser = User.objects.get(
+            username=test_superuser_username
+        )
         local_test_note = Note.objects.get(title=test_note_title)
         client.force_login(local_test_superuser)
-        response = client.get(reverse('file_manager:note_details', kwargs={'pk': local_test_note.id}))
-        self.assertTemplateUsed(response, template_name='templates/file_manager/note_detail.html')
+        response = client.get(
+            reverse(
+                'file_manager:note_details',
+                kwargs={'pk': local_test_note.id})
+        )
+        self.assertTemplateUsed(
+            response,
+            template_name='templates/file_manager/note_detail.html'
+        )
 
     def test_that_anon_users_are_redirected_from_note_detail_view(self):
         client = Client()
         # Pull note obj from db
         local_test_note = Note.objects.get(title=test_note_title)
-        response = client.get(reverse('file_manager:note_details', kwargs={'pk': local_test_note.id }))
-        expected_url = '/accounts/login/?next=%s' % reverse('file_manager:note_details')
+        response = client.get(
+            reverse(
+                'file_manager:note_details',
+                kwargs={'pk': local_test_note.id })
+        )
+        expected_url = ('/accounts/login/?next=%s' %
+                        reverse('file_manager:note_details'))
         self.assertRedirects(
             response=response,
             expected_url=expected_url
--- file_manager/urls.py
+++ file_manager/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
 
     # Note urls
-    url(r'^note/(?P<pk>[a-zA-Z0-9_-]+)/details/$', views.NoteDetailView.as_view(), name='note_details'),
+    url(
+        r'^note/(?P<pk>[a-zA-Z0-9_-]+)/details/$',
+        views.NoteDetailView.as_view(),
+        name='note_details'
+    ),
     url(r'^note/create', views.CreateNoteView.as_view(), name='note_create'),
     url(r'^notes/$', views.NoteIndexView, name='note_index'),
     # url(r'^note/update/(?P<pk>[a-zA-Z0-9_-]+)$', views.NoteUpdateView.as_view(), name='note_update')
